531b56557272fd81069b631e9f22d6e015678569
Josefine Bystrom <bystromjosefine@gmail.com>
2023-02-07 20:51:51 -0800

    Fixed functions again

--- HTML_Validator.py
+++ HTML_Validator.py
@@ -4,7 +4,10 @@ import re
 
 def validate_html(html):
     '''
-    This function performs a limited version of html validation by checking whether every opening tag has a corresponding closing tag.
+    This function performs a limited
+    version of html validation by
+    checking whether every opening tag
+    has a corresponding closing tag.
 
     >>> validate_html('<strong>example</strong>')
     True
@@ -13,42 +16,50 @@ def validate_html(html):
     '''
 
     # HINT:
-    # use the _extract_tags function below to generate a list of html tags without any extra text;
-    # then process these html tags using the balanced parentheses algorithm from the class/book
-    # the main difference between your code and the code from class will be that you will have to keep track of not just the 3 types of parentheses,
+    # use the _extract_tags function below to generate a
+    # list of html tags without any extra text;
+    # then process these html tags using the balanced
+    # parentheses algorithm from the class/book
+    # the main difference between your code and the
+    # code from class will be that you will have to keep
+    # track of not just the 3 types of parentheses,
     # but arbitrary text located between the html tags
 
-    index = 0
-    balanced = True
-    tags = _extract_tags(html)
-    stack = []
     if len(html) == 0:
         return True
-    while index < len(tags) and balanced is True:
-        if len(tags) == 0:
-            return True
+    index = 0
+    tags = _extract_tags(html)
+    stack = []
+    balanced = True
+    if not tags:
+        return False
+    while index < len(tags)and balanced:
         tag = tags[index]
-        endtag = str('</' + tag[1::])
+        tagname = tag[1:-1]
         if '/' not in tag:
-            stack.append(tag)
-            index += 1
+            stack.append(tagname)
         else:
-            if len(stack) == 0:
-                return False
-            index += 1
-        if endtag in tags:
-            stack.pop()
-            index += 1
-    if len(stack) == 0:
+            if not stack:
+                balanced = False
+            else:
+                popped = stack.pop()
+                if not popped == tagname[1:]:
+                    balanced = False
+        index += 1
+    if balanced and not stack:
         return True
+    else:
+        return False
 
 
 def _extract_tags(html):
     '''
     This is a helper function for `validate_html`.
-    By convention in Python, helper functions that are not meant to be used directly by the user are prefixed with an underscore.
+    By convention in Python, helper functions that are not meant to be
+    used directly by the user are prefixed with an underscore.
 
-    This function returns a list of all the html tags contained in the input string,
+    This function returns a list of all the html tags contained
+    in the input string,
     stripping out all text not contained within angle brackets.
 
     >>> _extract_tags('Python <strong>rocks</strong>!')
